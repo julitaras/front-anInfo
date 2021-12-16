c25560e4d106594dd4c842379151f140aa4391a2
Daniel <daanilo2497@gmail.com>
2021-12-16 12:43:36 -0300

    Actualizaciones

--- src/components/page/ProductsPage.js
+++ src/components/page/ProductsPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ProductsTable from "../table/ProductsTable"
 import ProductService from "../../service/ProductService";
+import Header from "./Header";
 
 class ProductsPage extends Component {
 
@@ -22,6 +23,7 @@ class ProductsPage extends Component {
     render() {
         return (
             <div>
+                <Header />
                 <ProductsTable products={this.state.products} />
             </div>);
     }
