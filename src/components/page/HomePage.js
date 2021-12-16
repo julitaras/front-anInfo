f3cadf2b014b7b397236e7b9b060bf61c719ab76
Mauro-Rizzi <mrizzi@fi.uba.ar>
2021-12-16 20:40:33 -0300

    Se retoca home page y se actualiza titulo del sitio

--- src/components/page/HomePage.js
+++ src/components/page/HomePage.js
@@ -1,24 +1,34 @@
 import React, {Component} from "react"
 import logo from '../../logo.svg';
 import Nav from "react-bootstrap/Nav";
+import {Container} from "react-bootstrap";
 
 class HomePage extends Component {
 
     render() {
         return (<div className="App">
+
           <header className="App-header">
-            <h1> PSA </h1>
+              <Container>
+                  <Nav className="me-xxl-auto nav-fill">
+                      <Nav.Link href="/products" >Modulo Soporte</Nav.Link>
+                      <Nav.Link href="/projects">Modulo Proyectos</Nav.Link>
+                      <Nav.Link href="/hours">Modulo Recursos</Nav.Link>
+                  </Nav>
+              </Container>
+            <h1> Sistema de gestión PSA </h1>
               <img src={logo} className="App-logo" alt="logo" />
             <p>
               Bienvenido al sitio web de PSA.
             </p>
           </header>
-            <Nav className="me-auto nav-fill ">
-                <Nav.Link href="/products">Modulo Soporte</Nav.Link>
-                <Nav.Link href="/projects">Modulo Proyectos</Nav.Link>
-                <Nav.Link href="/hours">Modulo Recursos</Nav.Link>
-            </Nav>
-          </div>);
+            <Container>
+
+            </Container>
+          </div>
+
+
+            );
     }
 }
 
