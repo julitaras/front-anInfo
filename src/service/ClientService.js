ac225b31c39854f3cd796d63f4a0cc6345bf2f59
Alejo Rodriguez <atrodriguez@fi.uba.ar>
2021-12-19 19:24:47 -0300

    update url de client service

--- src/service/ClientService.js
+++ src/service/ClientService.js
@@ -3,12 +3,12 @@ import axios from "axios";
 class ClientService {
 
     async getClients() {
-        const response = await axios.get("http://soporte-fiuba.herokuapp.com/clients");
+        const response = await axios.get("https://soporte-fiuba.herokuapp.com/clients");
         return response.data;  
     }
 
     async getClient(clientID) {
-        const response = await axios.get(`http://soporte-fiuba.herokuapp.com/clients/${clientID}`);
+        const response = await axios.get(`https://soporte-fiuba.herokuapp.com/clients/${clientID}`);
         return response.data;  
     }
 };
